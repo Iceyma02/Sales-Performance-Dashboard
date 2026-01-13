d212569daf43ad5fc43549524190a4766b9c9481
Anesu Manjengwa <manjengwap10@gmail.com>
2026-01-13 22:49:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import numpy as np
 from datetime import datetime
 import warnings
 warnings.filterwarnings('ignore')
-from pathlib import Path
+
 
 # Professional Business Colors
 COLOR_PALETTE = {
